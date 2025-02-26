64ab64ff8835cd67f4091d416093a1cb1c03d1e1
Berwyn Natanael <berwynnatanael@gmail.com>
2025-02-26 12:21:36 +0700

    Sesi 7-8
    Membetulkan ending scene
    Setup idle dan attack animation
    Scripting Change scene after second

---
+++ Assets/Scripts/ChangeSceneAfterSecond.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneAfterSecond : MonoBehaviour
+{
+    public LoadScene ls;
+    public float second;
+    public string sceneName;
+    void Start()
+    {
+        Invoke("ChangeAfterSecond", second);
+    }
+    private void ChangeAfterSecond()
+    {
+        ls.ChangeScene(sceneName);
+    }
+ 
+}
